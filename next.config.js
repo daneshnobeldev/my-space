688e85ba887d53473c4dee1cef4230e542f81f5d
daneshnobel <8457787+daneshnobel@users.noreply.github.com>
2023-06-12 11:20:27 +0530

    deploy fix#2

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withNextra = require('nextra')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // any configs you need
-  image : {unoptimized : true}
+  images : {unoptimized : true}
 }
 
 module.exports = withNextra(nextConfig)
